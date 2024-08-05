469c714b70757e3c927a184df8923e499c4877a4
윤종무 <buchonsi@naver.com>
2024-08-05 17:59:22 +0900

    first commit

---
+++ src/main/java/com/yoon/subwaytiming/SubwayTimingApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.subwaytiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayTimingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubwayTimingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yoon/subwaytiming/SubwayTimingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.subwaytiming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayTimingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
