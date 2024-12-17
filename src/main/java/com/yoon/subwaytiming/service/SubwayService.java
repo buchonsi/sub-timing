878c9522e7b116082f884d1ef105c29c1dfbad8d
buchonsi <buchonsi@naver.com>
2024-12-17 16:33:54 +0900

    test11111

--- src/main/java/com/yoon/subwaytiming/service/SubwayService.java
+++ src/main/java/com/yoon/subwaytiming/service/SubwayService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class SubwayService {
     public void test4() {
 
-        System.out.println("test4");
+        System.out.println("test41111");
     }
 }
