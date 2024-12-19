abc0601baf3d5e3df9205a5c912b6a6f0c7a76aa
buchonsi <buchonsi@naver.com>
2024-12-19 13:51:07 +0900

    test

--- src/main/java/com/yoon/subwaytiming/service/SubwayService.java
+++ src/main/java/com/yoon/subwaytiming/service/SubwayService.java
@@ -7,4 +7,14 @@ public class SubwayService {
     public void test3() {
         System.out.println("test3");
     }
+    public void service() {
+
+        int a = 100;
+        int b = 200;
+        int result = a + b;
+
+        System.out.println(result);
+
+
+    }
 }
